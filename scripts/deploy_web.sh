fae709db5bfdb624856693379e43c940a277a87c
Scott Laing <fifersheep@gmail.com>
2023-05-03 21:55:13 +0100

    download flutter in web deployment script

--- scripts/deploy_web.sh
+++ scripts/deploy_web.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+if [ ! -d "flutter" ]; then
+    git clone https://github.com/flutter/flutter.git
+fi
+
+cd flutter
+git checkout 3.7.12
+cd ..
+export PATH="$PATH:`pwd`/flutter/bin"
+
 flutter packages get
 flutter build web
