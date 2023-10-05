59901b15811f122a46b06131deb8073409dd3cdf
Scott Laing <fifersheep@gmail.com>
2023-10-06 00:08:48 +0100

    constrain width to 600

--- lib/src/presentation/content.dart
+++ lib/src/presentation/content.dart
@@ -25,51 +25,57 @@ class _ContentState extends State<Content> {
     return SafeArea(
       child: Container(
         color: ThemeColors.primary,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-              child: Text(
-                AppLocalizations.of(context)!.appTitle,
-                style: textStyle.copyWith(fontSize: 50),
-              ),
+        child: Align(
+          alignment: Alignment.center,
+          child: ConstrainedBox(
+            constraints: const BoxConstraints(maxWidth: 600),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  child: Text(
+                    AppLocalizations.of(context)!.appTitle,
+                    style: textStyle.copyWith(fontSize: 50),
+                  ),
+                ),
+                // Integrated the segmented control directly here
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: CupertinoSlidingSegmentedControl<Mode>(
+                      backgroundColor: ThemeColors.primaryDark,
+                      thumbColor: ThemeColors.primaryLight,
+                      groupValue: _selectedSegment,
+                      onValueChanged: (Mode? value) {
+                        if (value != null) {
+                          setState(() {
+                            _selectedSegment = value;
+                          });
+                        }
+                      },
+                      children: {
+                        Mode.now: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            AppLocalizations.of(context)!.now,
+                            style: const TextStyle(color: CupertinoColors.white),
+                          ),
+                        ),
+                        Mode.hours: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: Text(
+                            AppLocalizations.of(context)!.alarm,
+                            style: const TextStyle(color: CupertinoColors.white),
+                          ),
+                        ),
+                      }),
+                ),
+                const SizedBox(height: 16.0),
+                _selectedSegment == Mode.now ? const Now() : const Alarm(),
+              ],
             ),
-            // Integrated the segmented control directly here
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: CupertinoSlidingSegmentedControl<Mode>(
-                  backgroundColor: ThemeColors.primaryDark,
-                  thumbColor: ThemeColors.primaryLight,
-                  groupValue: _selectedSegment,
-                  onValueChanged: (Mode? value) {
-                    if (value != null) {
-                      setState(() {
-                        _selectedSegment = value;
-                      });
-                    }
-                  },
-                  children: {
-                    Mode.now: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        AppLocalizations.of(context)!.now,
-                        style: const TextStyle(color: CupertinoColors.white),
-                      ),
-                    ),
-                    Mode.hours: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        AppLocalizations.of(context)!.alarm,
-                        style: const TextStyle(color: CupertinoColors.white),
-                      ),
-                    ),
-                  }),
-            ),
-            const SizedBox(height: 16.0),
-            _selectedSegment == Mode.now ? const Now() : const Alarm(),
-          ],
+          ),
         ),
       ),
     );
