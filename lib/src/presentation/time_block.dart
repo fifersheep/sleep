b2a550fbe787e8083e092d83820af801dd0ddbc2
Scott Laing <fifersheep@gmail.com>
2023-10-05 23:49:55 +0100

    update time block layout

--- lib/src/presentation/time_block.dart
+++ lib/src/presentation/time_block.dart
@@ -9,36 +9,42 @@ class TimeBlock extends StatelessWidget {
   final Color color;
 
   @override
-  Widget build(BuildContext context) => Container(
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: color,
-            width: 0.5,
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(4)),
+  Widget build(BuildContext context) {
+    double textSize = 16;
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: color,
+          width: 0.5,
         ),
-        child: Stack(
-          alignment: Alignment.center,
-          clipBehavior: Clip.none,
-          children: [
-            Positioned(
-              left: -55,
-              child: Text(
-                cycles,
-                style: const TextStyle(
-                  fontSize: 80,
-                  color: ThemeColors.white5,
+        borderRadius: const BorderRadius.all(Radius.circular(4)),
+      ),
+      child: Stack(
+        alignment: Alignment.center,
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            top: -textSize / 2,
+            child: Container(
+              color: ThemeColors.primary,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 4),
+                child: Text(
+                  cycles,
+                  style: TextStyle(fontSize: textSize, color: color),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 32),
-              child: Text(
-                time,
-                style: TextStyle(color: color, fontSize: 30),
-              ),
-            )
-          ],
-        ),
-      );
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 32),
+            child: Text(
+              time,
+              style: TextStyle(color: color, fontSize: 30),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
